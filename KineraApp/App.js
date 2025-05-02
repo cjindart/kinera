3f18cd46daf03cdd1365fb3aaed0cb091343434b
csprout3 <csprout3@stanford.edu>
2025-05-01 19:43:51 -0700

    Update App.js to use the improved Layout component

--- KineraApp/App.js
+++ KineraApp/App.js
@@ -1,17 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Home from "./tabs/tabNav";
-
-const Stack = createNativeStackNavigator();
+import Layout from "./_layout";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Layout />;
 }
